aef17bd537275936d6348188e4897ced51f400b1
Alex Davidson <alex.davidson@warwick.ac.uk>
2022-10-14 15:20:32 +0000

    to test command line

---
+++ command_line_test.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <cmath>
+#include <string>
+#include <vector>
+
+
+
+int main(int argc, char* argv[])
+{
+    const std::vector<std::string> cmdLineArgs { argv, argv+argc };
+
+}
