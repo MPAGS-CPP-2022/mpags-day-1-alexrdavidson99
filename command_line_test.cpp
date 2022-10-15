92be20adb33efd0bae1678b23178e954005702d5
Alex Davidson <alex.davidson@warwick.ac.uk>
2022-10-15 16:04:18 +0000

    tomathis adds the help function to the command line and a -i -o but need to short order

--- command_line_test.cpp
+++ command_line_test.cpp
@@ -8,5 +8,39 @@
 int main(int argc, char* argv[])
 {
     const std::vector<std::string> cmdLineArgs { argv, argv+argc };
+    const std::string help = "there is no help for you!!!";
+    
+    for (int i = 0; i < cmdLineArgs.size(); i++)
+    {
+        std::cout << cmdLineArgs[i] << std::endl;
+      
+        if (cmdLineArgs[i] == "help") {
+
+        std::cout<< help << std::endl;
+        }
+        if (cmdLineArgs[i] == "--help") {
+
+        std::cout<< help  << std::endl;
+        }
+        if (cmdLineArgs[i] == "hp") {
+
+        std::cout<< help  << std::endl;
+        }
+        if (cmdLineArgs[i] == "--hp") {
+
+        std::cout<< help  << std::endl;
+        }
+
+    }
+
+    for (int i = 0; i < cmdLineArgs.size(); i++)
+    {
+        if (cmdLineArgs[i] == "-i" && cmdLineArgs[i+2] == "-o" ) {
+            std::cout<< cmdLineArgs[i+1] << std::endl;
+        
+        } else break;
+
+    }
+    std::cout<<"First arg: " << cmdLineArgs[0] << std::endl;
 
 }
