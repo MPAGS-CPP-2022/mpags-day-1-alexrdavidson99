973d51350603bb151f2d10948f8bc9cebd25eeaf
Alex Davidson <alex.davidson@warwick.ac.uk>
2022-10-18 14:17:06 +0000

    for real now final changes to ex8 work

--- command_line_test.cpp
+++ command_line_test.cpp
@@ -1,4 +1,3 @@
-#include <cmath>
 #include <iostream>
 #include <string>
 #include <vector>
@@ -7,8 +6,7 @@ int main(int argc, char* argv[])
 {
     const std::vector<std::string> cmdLineArgs{argv, argv + argc};
     const std::string help =
-        " there is no help for you!!!\n This is how the code will hopefully work in the in the end \n but at the moment i am just tring to see how the how vector thing works\
-        \nlook this is gett very big now is there a better way of doing this ";
+        " will add a more indepth help when code is updated";
     // outputs the help command 
     for (int i = 0; i < cmdLineArgs.size(); i++) {
         std::cout << cmdLineArgs[i] << std::endl;
